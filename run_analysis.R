240b8284f4215268b9c7b80faf9f15b313dab84c
Lev Taydakov <lev.taydakov@gmail.com>
2014-08-23 23:38:32 -0700

    refactored

--- run_analysis.R
+++ run_analysis.R
@@ -25,9 +25,10 @@ rm(list = c("test_labels", "test_subjects", "test_data", "train_labels", "train_
 # STEP 2
 ## Read features list and leave only columns with names contains "mean()" and "std()"
 features <- read.table("features.txt", colClasses = c("numeric", "character"))
-names(features) <- c("colnum", "colname")
-features <- features[grep("(mean)|(std)\\(\\)", features$colname),]
-full_data <- full_data[, features$colnum]
+names(features)  <- c("colnum", "colname")
+full_features    <- features
+features         <- features[grep("(mean)|(std)\\(\\)", features$colname),]
+meanstd_data <- full_data[, features$colnum]
 
 # STEP 3
 ## Read activity labels and map full_labels using factor
@@ -36,17 +37,19 @@ full_activities <- factor(full_labels, levels=activities$V1, labels=activities$V
 
 # STEP 4
 ## Use feature labels to name columns of the data set
-names(full_data) <- features$colname
+names(meanstd_data) <- features$colname
 
 # Create data set with all the columns inside, remove unused variables
-dataset <- cbind(full_activities, full_subjects, full_data)
+dataset <- cbind(full_activities, full_subjects, meanstd_data)
 names(dataset)[c(1,2)] <- c("activity", "subject")
-rm(list = c("full_labels", "full_subjects", "full_data", "full_activities", "activities"))
+write.csv(dataset, "fulldataset.csv")
 
 # STEP 5
 ## Creating new data set with the average of each variable for each activity and each subject by melting and then casting it
+newdataset <- cbind(full_activities, full_subjects, full_data)
+names(newdataset) <- c(c("activity", "subject"), full_features$colname)
 ## Melt the data set by all the columns except activity and subject
-datasetmelted <- melt(dataset, id=c("activity", "subject"), measure.vars = features$colname)
+datasetmelted <- melt(newdataset, id=c("activity", "subject"), measure.vars = full_features$colname)
 ## Cast the melted data set by activity and subject
 datasetcasted <- dcast(datasetmelted, activity + subject ~ variable, mean)
 ## The casted data set contains 180 observations (30 subjects by 6 different activities) and 81 columns
