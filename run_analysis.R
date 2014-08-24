21e861ff8438eb3af949724499491ce279cc3656
Lev Taydakov <lev.taydakov@gmail.com>
2014-08-23 23:02:54 -0700

    STEP 5. calculated average of each variable for each activity and each subject

--- run_analysis.R
+++ run_analysis.R
@@ -17,9 +17,10 @@ full_data     <- rbind(test_data,     train_data)$V1
 rm(list = c("test_labels", "test_subjects", "test_data", "train_labels", "train_subjects", "train_data"))
 
 # STEP 2
-## Read features list and leave only mean and std columns
+## Read features list and leave only columns with names contains "mean()" and "std()"
 features <- read.table("features.txt", colClasses = "character")
-full_data <- full_data[, features$V1[grep("(mean)|(std)\\(\\)", features$V2)]]
+features <- features[grep("(mean)|(std)\\(\\)", features$V2),]
+full_data <- full_data[, features$V1]
 
 # STEP 3
 ## Read activity labels and map full_labels using factor
@@ -28,5 +29,16 @@ full_activities <- factor(full_labels, levels=activities$V1, labels=activities$V
 
 # STEP 4
 ## Use feature labels to name columns of the data set
-names(full_data) <- features$V2[grep("(mean)|(std)\\(\\)", features$V2)]
+names(full_data) <- features$V2
 
+# Create data set with all the columns inside, remove unused variables
+dataset <- cbind(full_subjects, full_activities, full_data)
+names(dataset)[c(1,2)] <- c("subject", "activity")
+rm(list = c("full_labels", "full_subjects", "full_data", "full_activities", "activities"))
+
+# STEP 5
+## Melt the data set by all the columns except activity and subject
+datasetmelted <- melt(dataset, id=c("activity", "subject"), measure.vars = features$V2)
+## Cast the melted data set by activity and subject
+datasetcasted <- dcast(datasetmelted, activity + subject ~ variable, mean)
+## The casted data set contains 180 observations (30 subjects by 6 different activities) and 81 columns
